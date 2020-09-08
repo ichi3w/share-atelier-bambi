3e556b1767d044378467216091a42d250995f5d4
ichi <ghiroichi@gmail.com>
2020-09-08 18:17:21 +0900

    スクロールでアニメ用クラスを追加するjsを追加

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+jQuery(($) => {
+  $(window).scroll(() => {
+    $('.effect').each(function(){
+      let elemPos = $(this).offset().top;
+      let scroll = $(window).scrollTop();
+      let windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight){
+        $(this).addClass('is-active');
+      }
+    });
+　});
+});
+
